71cc61545dceb94ce032aee1264b59457d21edcb
sergo <sergolisitcyn@gmail.com>
2022-08-08 23:11:39 +0200

    json_decode

--- frontend/widgets/MfoViewWidget.php
+++ frontend/widgets/MfoViewWidget.php
@@ -22,7 +22,8 @@ class MfoViewWidget extends Widget
 
     public function run()
     {
-        $mfoText = MfoText::find()->where(['name' => 'Text'])->one();
+        $text = MfoText::find()->where(['name' => 'Text'])->one();
+        $mfoText = json_decode($text->text_mfo,true);
         if($this->type == 'reviews') {
             return $this->render('mfo/'.$this->type,[
                 'reviewsModel' => $this->reviewsModel,
@@ -54,7 +55,7 @@ class MfoViewWidget extends Widget
         }
         return $this->render('mfo/mfo-view/'.$this->type,[
             'model' => $this->model,
-            'mfoText' => $mfoText->text_mfo['text'],
+            'mfoText' => $mfoText['text'],
             'mfoRandoms' => $mfoRandoms,
         ]);
 
