0bf06d7ed0a37cc712dbd1322718fedfce1cab53
sergo <sergolisitcyn@gmail.com>
2022-12-13 20:47:41 +0200

    menu top

--- frontend/widgets/views/main-page/menu-top.php
+++ frontend/widgets/views/main-page/menu-top.php
@@ -1,17 +1,28 @@
 <div class="container">
     <ul class="sf-menu">
-        <li>
-            <a href="#">Еще</a>
-            <ul>
-                <?php foreach ($menus as $menu) :?>
-                <li>
-                    <a href="<?= $menu->link; ?>">
-                        <?= $menu->name; ?>
-                    </a>
-                </li>
-                <?php endforeach; ?>
-            </ul>
-        </li>
+        <li><a href="/prestamos_personales_urgentes">Prestamos personales urgentes</a></li>
+        <li><a href="/prestamos_rapidos">Prestamos rapidos</a></li>
+        <li><a href="/prestamos_en_linea_sin_buro">Prestamos en linea sin buró</a></li>
+        <li><a href="/prestamos_en_linea">Prestamos en linea</a></li>
+        <?php foreach ($menus as $menu) :?>
+            <li class="menu-top-desktop">
+                <a href="<?= $menu->link; ?>">
+                    <?= $menu->name; ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+<!--        <li>-->
+<!--            <a href="#">Еще</a>-->
+<!--            <ul>-->
+<!--                --><?php // foreach ($menus as $menu) :?>
+<!--                <li>-->
+<!--                    <a href="--><?php //$menu->link; ?><!--">-->
+<!--                        --><?php //$menu->name; ?>
+<!--                    </a>-->
+<!--                </li>-->
+<!--                --><?php //endforeach; ?>
+<!--            </ul>-->
+<!--        </li>-->
         <li class="phone"><a href="tel:+525590630252">+ 52 55 90 63 02 52</a></li>
     </ul>
 </div>
