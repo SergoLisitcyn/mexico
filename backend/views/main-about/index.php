cabde7f9095bbdc7291ccd804cced36ce5b9ca31
sergo <sergolisitcyn@gmail.com>
2022-12-21 23:54:50 +0200

    main-about

--- backend/views/main-about/index.php
+++ backend/views/main-about/index.php
@@ -88,8 +88,8 @@ $this->params['breadcrumbs'][] = $this->title;
                 'format' => 'raw',
                 'options' => ['width' => '200'],
                 'value' => function ($model, $index) {
-                    return Html::tag('a', 'Редактировать', ['href' => Url::toRoute(['main-partners/update', 'id' => $index]), 'class' => 'btn btn-success', 'style' => 'font-weight: 100;margin-right:10px'])
-                        .Html::tag('a', 'Удалить', ['href' => Url::toRoute(['main-partners/delete', 'id' => $index]), 'data-method' => 'post', 'data-confirm' => 'Вы точно хотите удалить?', 'class' => 'btn btn-order btn-danger', 'style' => 'font-weight: 100']);
+                    return Html::tag('a', 'Редактировать', ['href' => Url::toRoute(['main-about/update', 'id' => $index]), 'class' => 'btn btn-success', 'style' => 'font-weight: 100;margin-right:10px'])
+                        .Html::tag('a', 'Удалить', ['href' => Url::toRoute(['main-about/delete', 'id' => $index]), 'data-method' => 'post', 'data-confirm' => 'Вы точно хотите удалить?', 'class' => 'btn btn-order btn-danger', 'style' => 'font-weight: 100']);
                 },
             ],
         ],
