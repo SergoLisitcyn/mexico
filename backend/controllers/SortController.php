a8d8d2893b3383a321ea9744375ea1c10a59ea01
sergo <sergolisitcyn@gmail.com>
2023-10-31 21:24:50 +0200

    fix

--- backend/controllers/SortController.php
+++ backend/controllers/SortController.php
@@ -70,6 +70,11 @@ class SortController extends Controller
 
         $dataProvider = new ActiveDataProvider([
             'query' => Mfo::find()->where(['in', 'id', $str]),
+            'sort' => [
+                'defaultOrder' => [
+                    'id' => SORT_DESC,
+                ]
+            ],
         ]);
         $title = '';
         if($url == 'prestamos_rapidos'){
